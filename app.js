724c7f7d2552f45baada03c56540ba3d8409b0b2
Shilpe <penguns@naver.com>
2012-10-29 23:13:33 +0900

    타이틀 변경

--- app.js
+++ app.js
@@ -31,19 +31,19 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 var logo = '\n\n';
-logo += '=================================================================================\n\n'
-logo += '                OOOOOO,   OO~            OO                                      \n'
-logo += '                OO   OOO  OO~            OO                                      \n'
-logo += '                OO    OO                 OO                   ,OOO,              \n'
-logo += '                OO    OO  OO~  OOOOOOO   OO  ,O,  OOOOOO   O~OOO OOOO            \n'
-logo += '                OO   ,OO  OO~  OO   OO.  OO .O,  ,OO   OO  OO                    \n'
-logo += '                OOOOOOO   OO~  OO   OO.  OO O,   OO    OO  OO                    \n'
-logo += '                OO  OO.   OO~  OO   OO.  OOOO.   OOOOOOOO  OO                    \n'
-logo += '                OO  .OO   OO~  OO   OO.  OO OO   OO        OO                    \n'
-logo += '                OO   OO,  OO~  OO   OO.  OO  OO  OO.       OO                    \n'
-logo += '                OO   .OO  OO~  OO   OO.  OO  OOO  OO  OO,  OO                    \n'
-logo += '                OO    OOO OO~  OO   OO.  OO   OO   OOOO    OO                    \n\n'
-logo += '=================================================================================\n\n'
+logo += '=====================================================================\n\n'
+logo += '          OOOOOO,   OO~            OO                                \n'
+logo += '          OO   OOO  OO~            OO                                \n'
+logo += '          OO    OO                 OO                   ,OOO,        \n'
+logo += '          OO    OO  OO~  OOOOOOO   OO  ,O,  OOOOOO   O~OOO OOOO      \n'
+logo += '          OO   ,OO  OO~  OO   OO.  OO .O,  ,OO   OO  OO              \n'
+logo += '          OOOOOOO   OO~  OO   OO.  OO O,   OO    OO  OO              \n'
+logo += '          OO  OO.   OO~  OO   OO.  OOOO.   OOOOOOOO  OO              \n'
+logo += '          OO  .OO   OO~  OO   OO.  OO OO   OO        OO              \n'
+logo += '          OO   OO,  OO~  OO   OO.  OO  OO  OO.       OO              \n'
+logo += '          OO   .OO  OO~  OO   OO.  OO  OOO  OO  OO,  OO              \n'
+logo += '          OO    OOO OO~  OO   OO.  OO   OO   OOOO    OO              \n'
+logo += '=====================================================================\n\n'
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log(logo,"Express server listening on port ", app.get('port'));
